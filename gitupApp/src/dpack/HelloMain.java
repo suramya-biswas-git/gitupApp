8e979d4fcf095fdebbcbc020ab1d55bb83c0e65e
suramya-biswas-git <suramya.biswas@gmail.com>
2025-04-01 18:13:36 +0530

    3rd Version

---
+++ gitupApp/src/dpack/HelloMain.java
@@ -0,0 +1,9 @@
+package dpack;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Hi");
+	}
+
+}
--- gitupApp/src/mypack/MyDemo.java
+++ gitupApp/src/mypack/MyDemo.java
@@ -4,6 +4,7 @@ public class MyDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello everyone");
+		System.out.println("Today is APRIL FOOL Day");
 
 	}
 
